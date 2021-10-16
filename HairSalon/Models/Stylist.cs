7f81443a9507a21ab64bb9e3518a477b120a1be8
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-10-16 02:21:13 -0400

    complete basic setup and create database

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public string Name { get; set; }
+    public int StylistId { get; set; }
+    public string Specialty { get; set; }
+    
+
+  }
+}
